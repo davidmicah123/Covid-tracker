8a256903dc414824ab6e25545cb7167473aed22e
David Micah-Daniels <98872908+davidmicah123@users.noreply.github.com>
2022-07-28 21:18:53 +0100

    updating functionality

--- script.js
+++ script.js
@@ -26,3 +26,33 @@ const getGlobal = async function () {
     main.insertAdjacentHTML('beforeend', html)
 }
 getGlobal();
+
+
+
+searchBtn.addEventListener('click',
+    () => {
+        searchBtn.innerHTML = `<i class="fa fa-spinner fa-spin"></i> Searching`;
+        const getCountrySearchData = async function () {
+            const country = await fetch( `https://restcountries.com/v2/name/${countryInput.value}`);
+            const resp = await country.json();
+            const data = await resp;
+            console.log(data);
+            
+            const countryData = await fetch(`https://covid-19.dataflowkit.com/v1/${countryInput.value}`);
+            const resp2 = await countryData.json();
+            const data2 = await resp2;
+            const html = `        
+                <div class="card">
+                    <img src="${data[0].flag}" alt="" class="card_img">
+                    <div class="card_txt">
+                        <h2>${data2['Country_text']} Result</h2>
+                        <ul>
+                            <li>New Cases: <span class="main_stat">${data2['New Cases_text']}</span></li>
+                            <li>New Deaths: <span class="main_stat">${data2['New Deaths_text']}</span></li>
+                            <li>Total Cases: <span class="main_stat">${data2['Total Cases_text']}</span></li>
+                            <li>Total Deaths: <span class="main_stat">${data2['Total Deaths_text']}</span></li>
+                            <li>Total Recovered: <span class="main_stat">${data2['Total Recovered_text']}</span></li>
+                        </ul>
+                    </div>
+                </div>
+            `;
